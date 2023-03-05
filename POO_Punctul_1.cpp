b1fd559c873ac73745c8af0894748c800d81d2c1
BogdanPetcu85 <109615036+BogdanPetcu85@users.noreply.github.com>
2023-03-05 15:26:32 +0200

    Add files via upload

---
+++ POO_Punctul_1.cpp
@@ -0,0 +1,23 @@
+#include"pch.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+	int v;
+
+	char nume[20];
+
+	cout << "Scrieti nume : ";
+
+	cin >> nume;
+
+	cout << "Scrieti varsta : ";
+
+	cin >> v;
+
+	cout << "Nume : " << nume << endl << "Varsta : " << v;
+
+}
