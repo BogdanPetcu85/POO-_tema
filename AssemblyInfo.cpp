0488de20c15f32af4650dc064334b2b377908db2
BogdanPetcu85 <109615036+BogdanPetcu85@users.noreply.github.com>
2023-04-30 15:00:17 +0300

    Add files via upload

--- AssemblyInfo.cpp
+++ AssemblyInfo.cpp
@@ -6,15 +6,15 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-[assembly:AssemblyTitleAttribute(L"CreditOrganizer")] ;
-[assembly:AssemblyDescriptionAttribute(L"")] ;
-[assembly:AssemblyConfigurationAttribute(L"")] ;
-[assembly:AssemblyCompanyAttribute(L"")] ;
-[assembly:AssemblyProductAttribute(L"CreditOrganizer")] ;
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2023")] ;
-[assembly:AssemblyTrademarkAttribute(L"")] ;
-[assembly:AssemblyCultureAttribute(L"")] ;
+[assembly:AssemblyTitleAttribute(L"BigBank")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"BigBank")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2023")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
 
-[assembly:AssemblyVersionAttribute(L"1.0.*")] ;
+[assembly:AssemblyVersionAttribute(L"1.0.*")];
 
-[assembly:ComVisible(false)] ;
+[assembly:ComVisible(false)];
